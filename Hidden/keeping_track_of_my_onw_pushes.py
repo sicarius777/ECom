58c74fdf95deba5283c8c9673b3413d2df875bf5
cameronmcnz <you@example.com>
2024-08-10 15:25:11 -0400

    'K'

--- Hidden/keeping_track_of_my_onw_pushes.py
+++ Hidden/keeping_track_of_my_onw_pushes.py
@@ -4,4 +4,4 @@
 # So this is proof that github is not showing my pushes if anyone wants to argue it i pushed yesterday and its not showing up. Well a new file and a new commented out area did not show up I will try a photo today.
 # yeah i commited aug 4th and aug 5th nothing showed took a photo its in assets.
 
-# Its been a coupled days but i have hit 5 today. 1 2 3 
+# Its been a coupled days but i have hit 5 today. 1 2 3 4
